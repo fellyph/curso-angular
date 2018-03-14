89b06d1b2ef2c99e55e973d6e8a6b94391dde8a9
fellyph <fellyph.cintra@gmail.com>
2018-03-14 15:54:28 +0000

    adding event binding

--- src/app/test.component.ts
+++ src/app/test.component.ts
@@ -5,10 +5,22 @@ import { Component } from '@angular/core';
     template: `<div class="course">
                 <h1>Nome do curso: {{nomeCurso}}</h1>
                 <h2>Quantidade de vagas: {{vagas}} </h2>
+                <button class="btn" (click)="matricularAluno()">Matricular</button>
             </div>`
 })
 
 export class TestComponent {
-    nomeCurso = 'Angular 5';
-    vagas = 10;
+    nomeCurso: string;
+    vagas: number;
+
+    constructor() {
+        this.nomeCurso = 'Angular';
+        this.vagas = 10;
+    }
+
+    matricularAluno() {
+        if (this.vagas > 0) {
+            this.vagas--;
+        }
+    }
 }
