e09655ff12a50fc9b9f209fe19f9072052caed04
fellyph <fellyph.cintra@gmail.com>
2018-03-30 15:21:58 +0100

    removing old curso component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { CursoComponent } from './curso/curso.component';
 import { AppComponent } from './app.component';
 import { CursosComponent } from './cursos/cursos.component';
 import { CursosListaComponent } from './cursos/cursos-lista/cursos-lista.component';
 import { CursosItemComponent } from './cursos/cursos-item/cursos-item.component';
 
 @NgModule({
-  declarations: [CursoComponent, AppComponent, CursosComponent, CursosListaComponent, CursosItemComponent],
+  declarations: [AppComponent, CursosComponent, CursosListaComponent, CursosItemComponent],
   imports: [
     BrowserModule,
     FormsModule
