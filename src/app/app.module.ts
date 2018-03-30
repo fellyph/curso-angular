1f5e8855c52d26039cc35e4a382bb1dbda35e616
fellyph <fellyph.cintra@gmail.com>
2018-03-30 12:47:40 +0100

    refectoring curso model and add essential components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CursoComponent } from './curso/curso.component';
 import { AppComponent } from './app.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { CursosListaComponent } from './cursos/cursos-lista/cursos-lista.component';
+import { CursosItemComponent } from './cursos/cursos-item/cursos-item.component';
 
 @NgModule({
-  declarations: [CursoComponent, AppComponent],
+  declarations: [CursoComponent, AppComponent, CursosComponent, CursosListaComponent, CursosItemComponent],
   imports: [
     BrowserModule,
     FormsModule
--- src/app/cursos/curso.ts
+++ src/app/cursos/curso.ts
@@ -1,13 +1,9 @@
 export class Curso {
-  public nome: string;
-  public categoria: string;
-  public cargaHoraria: number;
-  public thumbnail: string;
-  public dataCurso: string;
-
-  constructor (nome: string, categoria: string,
-              cargaHoraria: number, thumbnail: string,
-              dataCurso: string) {
+  constructor (public nome: string,
+              public categoria: string,
+              public cargaHoraria: number,
+              public thumbnail: string,
+              public dataCurso: string) {
     this.nome = nome;
     this.categoria = categoria;
     this.cargaHoraria = cargaHoraria;
