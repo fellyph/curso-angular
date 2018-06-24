11bd994e963ca95db138ef9ddf94951a84bf680c
fellyph <fellyph.cintra@gmail.com>
2018-06-25 00:21:14 +0100

    adding not found route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<app-cursos></app-cursos>',
+  template: '<app-main></app-main>',
 })
 
 export class AppComponent { }
--- src/app/cursos/curso.ts
+++ src/app/cursos/curso.ts
@@ -1,9 +1,12 @@
 export class Curso {
-  constructor (public nome: string,
+  constructor (
+              public id: number,
+              public nome: string,
               public categoria: string,
               public cargaHoraria: number,
               public thumbnail: string,
-              public dataCurso: string) {
+              public dataCurso: string,
+              public descricao: string = '') {
     this.nome = nome;
     this.categoria = categoria;
     this.cargaHoraria = cargaHoraria;
